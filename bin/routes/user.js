44c077f096423ad5b6be20e029d8e0b81acf5573
kphacko <karan2000patil@gmail.com>
2020-07-19 20:43:32 +0530

    new ui changes

--- bin/routes/user.js
+++ bin/routes/user.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controller/user');
+const { checkAdmin } = require('../middleware/auth');
+
 //some action to login user
 router.post('/makeBet', userController.beting);
 router.post('/gameHistory', userController.gameHistory);
 // router.post('/myHistory', userController.myHistory);
-
+router.get('/logout', (req, res, next) => { checkAdmin(req, res, next, ['user'], 'login') }, (req, res) => {
+    req.session.destroy();
+    res.redirect('/user/login');
+});
 router.post('/login', userController.login);
 router.get('/login', (req, res) => {
     res.render('login', { data: 'empty' });
