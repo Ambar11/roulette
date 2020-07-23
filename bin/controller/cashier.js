53fd90c236777a08bc14c4d3f1ad67b25acc9ef7
kphacko <karan2000patil@gmail.com>
2020-07-23 15:13:12 +0530

    cashier pages bug solved

--- bin/controller/cashier.js
+++ bin/controller/cashier.js
@@ -7,7 +7,9 @@ exports.makeTransaction = async(req, res) => {
     const { user_id, amount } = req.body;
     let query = '';
     try {
-        if (!user_id || !amount || user_id.length < 10) throw new Custom('User is not selected', 'Please select user or entered amount is invalid', 417);
+        if (!user_id || user_id.length < 10) throw new Custom('User is not selected', 'Please select user ', 417);
+        if (!amount) throw new Custom('User enter a valid amount', 'User enter a valid amount', 417);
+
         let userArray = await functions.querySingle(`SELECT * from user WHERE username = ${user_id}`);
         if (!userArray[0]) throw customError.userNotFound;
         if (userArray[0].status != 0) throw new Custom('Opps!', 'The user is unathorized!', '401');
@@ -22,6 +24,8 @@ exports.makeTransaction = async(req, res) => {
 
         } else if (req.query.status == 'debit') {
             if (parseInt(uPoints[0].points) == 0) throw new Custom('Opps!!', 'The user Points is Zero', '401');
+            if (parseInt(uPoints[0].points) < parseInt(amount)) throw new Custom(`Opps!! There is no enough Coins to withdraw The user balance is ${uPoints[0].points}`, 'points is less than balance coins ', '402');
+
             let totalPoints = parseInt(uPoints[0].points) - parseInt(amount);
             query = `UPDATE points SET points = ${totalPoints} WHERE id=${uPoints[0].id}`;
             await functions.querySingle(`INSERT INTO transaction (refrence,cashier_id,type,points,status,date) VALUES (${userArray[0].id},${req.session.u_id},'CASHIER',${parseInt(amount)},'DEBITED','${Date()}')`);
