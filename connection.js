a1a6d0020b4fb9fcfec798a0139a3aa54b6ea1f2
kphacko <karan2000patil@gmail.com>
2020-08-11 09:04:00 +0000

    database location changed

--- connection.js
+++ connection.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-    // host : 'localhost',
-    // user : 'root',
-    // password : '',
-    // database : 'bhajiwaala',
-    host: '107.178.108.59',
-    user: 'daxyin_beting',
-    password: 'DX7@karan',
-    database: 'daxyin_beting',
+    host : 'localhost',
+    user : 'root',
+    password : 'DX7@karan',
+    database : 'beting',
+    // host: '107.178.108.59',
+    // user: 'daxyin_beting',
+    // password: 'DX7@karan',
+    // database: 'daxyin_beting',
     multipleStatements: true
 });
 
